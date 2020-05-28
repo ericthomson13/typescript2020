482083dbf9ef5561e1b40bdd5fa1ee12ade6cfdb
et <eric.thomson13@gmail.com>
2020-05-28 10:57:41 -0600

    Combine Num or String Updated for Literal Input

--- basics/combineNumOrString.js
+++ basics/combineNumOrString.js
@@ -1,17 +1,12 @@
 function combineNumOrString(input1, input2, resultType) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
-    if (resultType === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+    return result;
 }
 var combinedAges = combineNumOrString(30, 26, 'as-number');
 var combinedNames = combineNumOrString('Eric', 'Thomson', 'as-string');
--- basics/combineNumOrString.ts
+++ basics/combineNumOrString.ts
@@ -1,16 +1,12 @@
-function combineNumOrString (input1: number | string, input2: number | string, resultType: string): number | string {
+function combineNumOrString (input1: number | string, input2: number | string, resultType: 'as-number' | 'as-string'): number | string {
   let result;
 
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  if (resultType === 'as-number') {
-    return +result;
-  } else {
-    return result.toString();
-  }
+  return result;
 }
 
 const combinedAges = combineNumOrString(30, 26, 'as-number');
